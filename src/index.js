2cac8c0b95ee86f3fa44524c839ea418f411ca0f
AAArtemenko <Ar4ys@yandex.ru>
2019-03-24 22:42:03 +0300

    feat: implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  var sum = 0, j = 1, i = 2, max = 0, a = new Array();
+ 
+  do {
+    if (base % i == 0) {
+      a[j] = i;
+      j++;
+      base = base / i;
+    }
+    else {
+      i++;
+    }
+  }
+  while (i < base);
+  a[j] = i; 
+
+  for (i = 1; i < a.length; ++i) {
+    if (a[i] > max) max = a[i];
+  }
+
+    for (var i = 1; i < 14; i++) {
+      sum += Math.floor(number / Math.pow(max, i));
+    }  
+
+  return sum;
+};
